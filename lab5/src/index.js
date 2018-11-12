e0943c422fe53cd59c5d9b80cb4573074783d0aa
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2018-11-12 16:40:12 +0000

    Mesh engine now has a draw

--- lab5/src/index.js
+++ lab5/src/index.js
@@ -18,19 +18,11 @@ const vertexPositions = new Float32Array([
   1.0,
 ]);
 
-let program = null;
-let positionBufferObject = null;
-let triangleMesh = null;
+main();
 
-const context = init();
-
-setInterval(() => {
-  display(context.gl);
-}, 1000);
-
-function init() {
+function main() {
   const context = new Context("glCanvas");
-  triangleMesh = new Mesh(context, { vertexPositions });
+  const triangleMesh = new Mesh(context, { vertexPositions });
   triangleMesh.initBuffers();
 
   /* Build both shaders */
@@ -47,43 +39,35 @@ function init() {
 
   /* Create a shader program object and link the vertex and fragment shaders
  	into a single shader program */
-  program = new Program(context.gl);
+  const program = new Program(context.gl);
   program.attachShader(vertShader);
   program.attachShader(fragShader);
   program.linkProgram();
 
-  return context;
-}
+  let then = 0;
+  function render(now) {
+    now *= 0.001;
+    const deltaTime = now - then;
+    then = now;
+    display(deltaTime);
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
 
-/**
- *
- * @param {WebGLRenderingContext} gl
- */
-function display(gl) {
-  /* Update the vertext buffer object with the modified array of vertices */
-  gl.bufferData(gl.ARRAY_BUFFER, triangleMesh.vertexPositions, gl.DYNAMIC_DRAW);
+  function display(deltaTime) {
+    const { gl } = context;
 
-  /* Define the background colour*/
-  gl.clearColor(1, 1, 1, 1);
-  gl.clear(gl.COLOR_BUFFER_BIT);
+    /* Define the background colour*/
+    gl.clearColor(1, 1, 1, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
 
-  /* Set the current shader program to be used */
-  gl.useProgram(program.program);
+    /* Set the current shader program to be used */
+    gl.useProgram(program.program);
 
-  /* Set the current active buffer object */
-  gl.bindBuffer(gl.ARRAY_BUFFER, triangleMesh.positionBufferObject);
+    triangleMesh.draw();
 
-  /* Specifies where the dat values accociated with index can accessed in the vertex shader */
-  gl.vertexAttribPointer(0, 4, gl.FLOAT, false, 0, 0);
-
-  /* Enable  the vertex array associated with the index*/
-  gl.enableVertexAttribArray(0);
-
-  /* Constructs a sequence of geometric primitives using the elements from the currently
- 	   bound matrix */
-  gl.drawArrays(gl.TRIANGLES, 0, 3);
-
-  /* Disable vertex array and shader program */
-  gl.disableVertexAttribArray(0);
-  gl.useProgram(null);
+    /* Disable vertex array and shader program */
+    gl.disableVertexAttribArray(0);
+    gl.useProgram(null);
+  }
 }
